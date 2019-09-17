0a2738ecafaad5233824257e07a39acba6538532
alaash3lan <sh3lan.a11@yahoo.com>
2019-09-17 16:49:38 +0200

    fix bugs

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-require_once "vendor/autoload.php" ;
-require_once "core/helpers.php";
-require_once "bootstrap.php";
+require_once __DIR__ . "/vendor/autoload.php" ;
+require_once __DIR__ . "/core/helpers.php";
+require_once __DIR__ . "/bootstrap.php";
 
 require_once 'routes/route.php' ;
 $app->router->load();
